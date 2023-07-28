d8679625a4e15e87db0e818ca41be5ef7a0bb49c
Zhakav Hesari <ZhakavHesari@gmail.com>
2023-07-28 17:40:38 +0330

    Add get all beers function to beer controller & beer service

--- src/main/java/zhakav/springframework/springrestmvc/controller/BeerController.java
+++ src/main/java/zhakav/springframework/springrestmvc/controller/BeerController.java
@@ -3,17 +3,26 @@ package zhakav.springframework.springrestmvc.controller;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import zhakav.springframework.springrestmvc.model.Beer;
 import zhakav.springframework.springrestmvc.service.BeerService;
 
+import java.util.List;
 import java.util.UUID;
 @Slf4j
-@Controller
+@RestController
 @AllArgsConstructor
 public class BeerController {
     private BeerService beerService;
+    @RequestMapping("/api/v1/beer")
+    public List<Beer> getAll(){
 
-    Beer getByID(UUID id){
+        return beerService.getAll();
+
+    }
+    @RequestMapping("")
+    public Beer getByID(UUID id){
 
         log.debug("GET BEER BY ID -IN BEER CONTROLLER -ID : " + id);
 
--- src/main/java/zhakav/springframework/springrestmvc/service/BeerService.java
+++ src/main/java/zhakav/springframework/springrestmvc/service/BeerService.java
@@ -2,8 +2,11 @@ package zhakav.springframework.springrestmvc.service;
 
 import zhakav.springframework.springrestmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+    List<Beer> getAll();
+
     Beer getByID(UUID id);
 }
