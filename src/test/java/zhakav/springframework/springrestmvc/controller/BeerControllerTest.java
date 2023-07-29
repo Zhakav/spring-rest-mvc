f76f39cf377efab4a0390204be18bdc93cc612da
Zhakav Hesari <ZhakavHesari@gmail.com>
2023-07-29 18:35:41 +0330

    Add Junit5 test for get all beers function

--- src/test/java/zhakav/springframework/springrestmvc/controller/BeerControllerTest.java
+++ src/test/java/zhakav/springframework/springrestmvc/controller/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package zhakav.springframework.springrestmvc.controller;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,6 +11,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import zhakav.springframework.springrestmvc.model.Beer;
 import zhakav.springframework.springrestmvc.model.BeerStyle;
 import zhakav.springframework.springrestmvc.service.BeerService;
+import zhakav.springframework.springrestmvc.service.BeerServiceImpl;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.hamcrest.core.Is.is;
@@ -18,7 +20,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.UUID;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -30,10 +32,14 @@ class BeerControllerTest {
     MockMvc mockMvc;
     @MockBean
     BeerService beerService;
-    @Test
-    void getByID() throws Exception {
+    List<Beer> beers;
+    @BeforeEach
+    void setup(){
 
-        Beer beerTest=Beer.builder()
+        Map<UUID,Beer>beerMap=new HashMap<>();
+
+
+        Beer beer1 = Beer.builder()
                 .id(UUID.randomUUID())
                 .version(1)
                 .beerName("Galaxy Cat")
@@ -45,6 +51,41 @@ class BeerControllerTest {
                 .updateDate(LocalDateTime.now())
                 .build();
 
+        Beer beer2 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Crank")
+                .beerStyle(BeerStyle.PALE_ALE)
+                .upc("12356222")
+                .price(new BigDecimal("11.99"))
+                .quantityOnHand(392)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        Beer beer3 = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Sunshine City")
+                .beerStyle(BeerStyle.IPA)
+                .upc("12356")
+                .price(new BigDecimal("13.99"))
+                .quantityOnHand(144)
+                .createdDate(LocalDateTime.now())
+                .updateDate(LocalDateTime.now())
+                .build();
+
+        beerMap.put(beer1.getId(),beer1);
+        beerMap.put(beer2.getId(),beer2);
+        beerMap.put(beer3.getId(),beer3);
+
+        beers=new ArrayList<>(beerMap.values());
+    }
+    @Test
+    void getByID() throws Exception {
+
+        Beer beerTest=beers.get(0);
+
         given(beerService.getByID(beerTest.getId())).willReturn(beerTest);
 
         mockMvc.perform(get("/api/v1/beer/" + beerTest.getId())
@@ -53,6 +94,19 @@ class BeerControllerTest {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.id", is(beerTest.getId().toString() )))
                 .andExpect(jsonPath("$.beerName", is(beerTest.getBeerName())));
+
+    }
+    @Test
+    void getAll() throws Exception{
+
+        given(beerService.getAll()).willReturn(beers);
+
+        mockMvc.perform(get("/api/v1/beer")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.length()",is(3)));
+
 
     }
 }
