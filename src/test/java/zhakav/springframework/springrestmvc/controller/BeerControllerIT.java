562c890c6606425e9ea5b103f6e01e0058d4de13
Zhakav Hesari <ZhakavHesari@gmail.com>
2023-08-02 21:57:27 +0330

    Add getAll cases to beer controller integration test

--- src/test/java/zhakav/springframework/springrestmvc/controller/BeerControllerIT.java
+++ src/test/java/zhakav/springframework/springrestmvc/controller/BeerControllerIT.java
@@ -1,8 +1,16 @@
 package zhakav.springframework.springrestmvc.controller;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import zhakav.springframework.springrestmvc.model.BeerDTO;
 import zhakav.springframework.springrestmvc.repository.BeerRepository;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +23,25 @@ class BeerControllerIT {
     @Autowired
     BeerRepository beerRepository;
 
-    
+    @Test
+    public void listBeers(){
+
+        List<BeerDTO> listOfBeers=beerController.getAll();
+
+        assertThat(listOfBeers.size()).isEqualTo(3);
+
+    }
+
+    @Test
+    @Rollback
+    @Transactional
+    public void emptyListBeers(){
+
+        beerRepository.deleteAll();
+        List<BeerDTO> listOfBeers=beerController.getAll();
+
+        assertThat(listOfBeers.size()).isEqualTo(0);
+
+    }
 
 }
