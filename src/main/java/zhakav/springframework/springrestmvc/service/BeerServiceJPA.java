dfe89f13f0bad1d858986d195e8241c84e66c087
Zhakav Hesari <ZhakavHesari@gmail.com>
2023-08-02 21:26:34 +0330

    Implement get methods in beer jpa service

--- src/main/java/zhakav/springframework/springrestmvc/service/BeerServiceJPA.java
+++ src/main/java/zhakav/springframework/springrestmvc/service/BeerServiceJPA.java
@@ -10,6 +10,7 @@ import zhakav.springframework.springrestmvc.repository.BeerRepository;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -20,12 +21,19 @@ public class BeerServiceJPA implements BeerService {
 
     @Override
     public List<BeerDTO> getAll() {
-        return null;
+        return beerRepository.findAll()
+                .stream()
+                .map(beerMapper::beerToBeerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public Optional<BeerDTO> getByID(UUID id) {
-        return Optional.empty();
+
+        return Optional.ofNullable(
+                beerMapper.beerToBeerDTO(
+                        beerRepository.findById(id)
+                                .orElse(null)));
     }
 
     @Override
