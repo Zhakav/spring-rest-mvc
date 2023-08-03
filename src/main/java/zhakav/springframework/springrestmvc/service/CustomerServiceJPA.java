52e69be3a7ee87b2a60a0fef4b374048d8a17d7f
Zhakav Hesari <ZhakavHesari@gmail.com>
2023-08-03 21:58:51 +0330

    Implement customer JPA service methods

--- src/main/java/zhakav/springframework/springrestmvc/service/CustomerServiceJPA.java
+++ src/main/java/zhakav/springframework/springrestmvc/service/CustomerServiceJPA.java
@@ -3,13 +3,18 @@ package zhakav.springframework.springrestmvc.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
+import zhakav.springframework.springrestmvc.exception.NotFoundException;
 import zhakav.springframework.springrestmvc.mapper.CustomerMapper;
+import zhakav.springframework.springrestmvc.model.BeerDTO;
 import zhakav.springframework.springrestmvc.model.CustomerDTO;
 import zhakav.springframework.springrestmvc.repository.CustomerRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 @Service
 @Primary
@@ -19,22 +24,46 @@ public class CustomerServiceJPA implements CustomerService {
     private final CustomerMapper customerMapper;
     @Override
     public Optional<CustomerDTO> getById(UUID id) {
-        return Optional.empty();
+
+        return Optional.ofNullable(
+                customerMapper.customerToCustomerDTO(
+                        customerRepository.findById(id)
+                                .orElse(null)));
+
     }
 
     @Override
     public List<CustomerDTO> getAll() {
-        return null;
+
+        return customerRepository.findAll()
+                .stream()
+                .map(customerMapper::customerToCustomerDTO)
+                .collect(Collectors.toList());
     }
 
     @Override
     public CustomerDTO save(CustomerDTO customer) {
-        return null;
+        return customerMapper.customerToCustomerDTO(
+                customerRepository.save(customerMapper.customerDTOTOCustomer(customer))) ;
     }
 
     @Override
     public Optional<CustomerDTO> updateById(CustomerDTO customer, UUID id) {
-        return Optional.empty();
+        AtomicReference<Optional<CustomerDTO>> atomicReference= new AtomicReference<>();
+
+        customerRepository.findById(id).ifPresentOrElse(foundCustomer->{
+
+            foundCustomer.setName(customer.getName());
+            foundCustomer.setUpdateDate(LocalDateTime.now());
+
+            atomicReference.set(Optional.of(customerMapper.customerToCustomerDTO(customerRepository.save(foundCustomer))));
+        },()->{
+            throw new NotFoundException();
+            //atomicReference.set(Optional.empty());
+        });
+
+        return Optional.ofNullable(customerMapper.customerToCustomerDTO(customerRepository.findById(id).get()));
+
     }
 
     @Override
@@ -44,6 +73,14 @@ public class CustomerServiceJPA implements CustomerService {
 
     @Override
     public Optional<CustomerDTO> deleteById(UUID id) {
-        return Optional.empty();
+
+        Optional<CustomerDTO> customerDTO= Optional.ofNullable(customerMapper.customerToCustomerDTO(customerRepository.findById(id).orElseThrow(() -> {
+            throw new NotFoundException();
+        })));
+
+        customerRepository.deleteById(id);
+
+        return customerDTO;
+
     }
 }
