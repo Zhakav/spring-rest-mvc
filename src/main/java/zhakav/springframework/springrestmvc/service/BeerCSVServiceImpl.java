e9e36d3b667833671f86afc1e11b16003497bf21
Zhakav Hesari <ZhakavHesari@gmail.com>
2023-08-05 21:01:13 +0330

    Save csv data to database using bootstarpData class

--- src/main/java/zhakav/springframework/springrestmvc/service/BeerCSVServiceImpl.java
+++ src/main/java/zhakav/springframework/springrestmvc/service/BeerCSVServiceImpl.java
@@ -1,6 +1,7 @@
 package zhakav.springframework.springrestmvc.service;
 
 import com.opencsv.bean.CsvToBeanBuilder;
+import org.springframework.stereotype.Service;
 import zhakav.springframework.springrestmvc.model.BeerCSVRecord;
 
 import java.io.File;
@@ -8,6 +9,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
+@Service
 public class BeerCSVServiceImpl implements BeerCSVService {
     @Override
     public List<BeerCSVRecord> convertCSV(File csvFile) {
