9426e951aec28a7de740d15e49c2d60daf557851
Zhakav Hesari <ZhakavHesari@gmail.com>
2023-08-06 10:22:03 +0330

    Setup get all beers by name method in beer repository

--- src/main/java/zhakav/springframework/springrestmvc/repository/BeerRepository.java
+++ src/main/java/zhakav/springframework/springrestmvc/repository/BeerRepository.java
@@ -3,7 +3,10 @@ package zhakav.springframework.springrestmvc.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import zhakav.springframework.springrestmvc.entity.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
+    List<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName);
+
 }
